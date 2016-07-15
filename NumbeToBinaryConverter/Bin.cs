aa73ea544acbc57977f2ca42a03803af64c91717
Ronald Albert <ronaldalbert1609@gmail.com>
2016-07-15 12:13:10 -0300

    MakingaDecimalBAseCalculator

---
+++ NumbeToBinaryConverter/Bin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbeToBinaryConverter
+{
+    static class Bin
+    {   
+        public static string numberToBin(ulong n)
+        {
+            string bin = "";
+            if(n < 0)
+            {
+                throw new NegativeNumberException();
+            }
+            if (n == 0)
+            {
+                return "0";
+            }
+            while (n != 1)
+            {
+                bin += Convert.ToString(n % 2);
+                n = n / 2;
+            }
+            bin += "1";
+            char[] bina = new char[bin.Length];
+            bina = bin.ToCharArray();
+            Array.Reverse(bina);
+            return new string(bina);
+        }
+        public static string numberToHexadecimal(ulong n)
+        {
+            List<string> hexa = new List<string>();
+            string hexad = "";
+            int number = 0;
+            int counter = 0;
+            if(n < 0)
+            {
+                throw new NegativeNumberException();
+            }
+            while (n != 0)
+            {
+                if (number > 15)
+                {
+                    number = 0;
+                    counter++;
+                }
+                if (number == 0)
+                {
+                    hexa.Add(Convert.ToString(number));
+                }
+                hexa[0] = Convert.ToString(number);
+                number++;
+                n--;
+            }
+            foreach(string i in hexa)
+            {
+                hexad += i;
+            }
+            return hexad;
+        }
+    }
+}
---
+++ NumbeToBinaryConverter/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumbeToBinaryConverter
+{
+    public partial class Form1 : Form
+    {
+        ulong number;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number = Convert.ToUInt64(textBox1.Text);
+                textBox2.Text = Convert.ToString(Bin.numberToBin(number));
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Esse caractere não é um numero");
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show("Número Não Aceito");
+            }
+        }
+    }
+}
